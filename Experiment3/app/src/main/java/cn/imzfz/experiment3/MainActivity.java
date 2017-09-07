9cf5ace48c744bebc2dc7f1aef0b0925ee4c6e2e
imzfz <admin@imzfz.cn>
2017-09-07 13:36:15 +0800

    implicit Intent

---
+++ Experiment3/app/src/main/java/cn/imzfz/experiment3/MainActivity.java
@@ -0,0 +1,30 @@
+package cn.imzfz.experiment3;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Intent intent;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button)findViewById(R.id.btn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent = new Intent();
+                intent.setAction("startActivity");
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ Experiment3/app/src/main/java/cn/imzfz/experiment3/TestActivity.java
@@ -0,0 +1,17 @@
+package cn.imzfz.experiment3;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by zfz on 2017/9/7.
+ */
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_test);
+    }
+}
