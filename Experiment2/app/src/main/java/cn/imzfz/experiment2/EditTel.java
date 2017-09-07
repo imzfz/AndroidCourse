3c41cdd35e6ef634b9bbc0e3602c2ff04ccb3e4b
imzfz <admin@imzfz.cn>
2017-09-07 08:40:38 +0800

    Activity

--- Experiment2/app/src/main/java/cn/imzfz/experiment2/EditTel.java
+++ Experiment2/app/src/main/java/cn/imzfz/experiment2/EditTel.java
@@ -2,22 +2,23 @@ package cn.imzfz.experiment2;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 /**
- * Created by zfz on 2017/9/6.
+ * Created by zfz on 2017/9/7.
  */
 
-public class EditName extends AppCompatActivity {
+public class EditTel extends AppCompatActivity {
 
-    String name;
-    EditText editName;
+    String tel;
+    EditText editTel;
     Intent intent;
     Button confirm;
+    TextView tv;
 
 
     @Override
@@ -25,16 +26,18 @@ public class EditName extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_login);
         intent = getIntent();
-        editName = (EditText) findViewById(R.id.edit_name);
+        tv = (TextView)findViewById(R.id.tips);
+        tv.setText(R.string.edit_tel);
+        editTel = (EditText) findViewById(R.id.edit_name);
         confirm = (Button) findViewById(R.id.confirm);
-        name = intent.getStringExtra("name");
-        editName.setText(name);
+        tel = intent.getStringExtra("tel");
+        editTel.setText(tel);
 
         confirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                intent.putExtra("res", editName.getText().toString());
-                setResult(0, intent);
+                intent.putExtra("res", editTel.getText().toString());
+                setResult(1, intent);
                 finish();
             }
         });
--- Experiment2/app/src/main/java/cn/imzfz/experiment2/MainActivity.java
+++ Experiment2/app/src/main/java/cn/imzfz/experiment2/MainActivity.java
@@ -3,6 +3,7 @@ package cn.imzfz.experiment2;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,24 +11,52 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     Intent intent;
-    Button edit;
+    Intent intent1;
+    Button editName;
+    Button editTel;
     TextView user;
+    TextView tel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         user = (TextView)findViewById(R.id.user_name);
-        edit = (Button)findViewById(R.id.btn);
+        tel = (TextView)findViewById(R.id.tel_number);
+        editName = (Button)findViewById(R.id.btn);
+        editTel = (Button)findViewById(R.id.edit_tel);
         intent = new Intent(this, EditName.class);
+        intent1 = new Intent(this, EditTel.class);
 
-        edit.setOnClickListener(new View.OnClickListener() {
+        editName.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 intent.putExtra("name", user.getText().toString());
                 startActivityForResult(intent, 0);
             }
         });
+
+        editTel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent1.putExtra("tel", tel.getText().toString());
+                startActivityForResult(intent1, 0);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 0 && resultCode == 0){
+            String res = data.getStringExtra("res");
+            user.setText(res);
+        }
+        if(requestCode == 0 && resultCode == 1){
+            String res = data.getStringExtra("res");
+            tel.setText(res);
+        }
     }
 
 }
